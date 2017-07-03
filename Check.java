0ccb032c8c471f79c6481f6e788f65ee4d750cbb
Psivaranjani <sathyapalanichamy90@gmail.com>
2017-07-03 14:50:38 +0530

    Create Check.java

---
+++ Check.java
@@ -0,0 +1,19 @@
+# positive
+import java.io.*;
+import java.util.*;
+public class Check{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int a=sc.nextInt();
+int x=a%2;
+if(x==0)
+{
+System.out.println("even");
+}
+else
+{
+System.out.println("odd");
+}
+}
+}
